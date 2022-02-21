60d4335388fdbbca418a1f0bca15d93049736cf3
Maciek Pekala <maciek@pleo.io>
2022-02-21 21:22:52 +0100

    fix: Fix description posting (#3)

--- actions/post-preview-urls/main.ts
+++ actions/post-preview-urls/main.ts
@@ -4,6 +4,7 @@
 
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+import {stripIndents as strip} from 'common-tags'
 
 import {getSanitizedBranchName, runAction} from '../utils'
 
@@ -12,15 +13,15 @@ runAction(async () => {
     const domain = core.getInput('domain', {required: true})
     const permalink = core.getInput('permalink')
     const repo = github.context.repo
-    const pr = github.context.payload.pull_request
+    const prNumber = github.context.payload.pull_request?.number
 
-    await postPreviewUrls({domain, permalink, token, pr, repo})
+    await postPreviewUrls({domain, permalink, token, prNumber, repo})
 })
 
 interface PostPreviewUrlsActionArgs {
     token: string
     domain: string
-    pr: typeof github.context.payload.pull_request
+    prNumber?: number
     repo: typeof github.context.repo
     permalink?: string
 }
@@ -29,34 +30,43 @@ export async function postPreviewUrls({
     token,
     domain,
     repo,
-    pr,
+    prNumber,
     permalink
 }: PostPreviewUrlsActionArgs) {
-    if (!pr) {
+    if (!prNumber) {
         throw new Error('Called outside of a PR context.')
     }
 
     const octokit = github.getOctokit(token)
-    const branchName = getSanitizedBranchName(pr.head.ref)
+
+    const latestPR = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
+        ...repo,
+        pull_number: prNumber
+    })
+    const branchName = getSanitizedBranchName(latestPR.data.head.ref)
+    const prBody = latestPR.data.body
 
     const markerStart = `<!--${domain}-preview-urls-do-not-change-below-->`
     const markerEnd = `<!--${domain}-preview-urls-do-not-change-above-->`
-    const freshPR = !pr.body?.includes(markerStart)
-    const prDescriptionAbove = pr.body?.split(markerStart)[0]?.trim() ?? ''
-    const prDescriptionBelow = pr.body?.split(markerEnd).pop()?.trim() ?? ''
+    const isFreshPR = !prBody?.includes(markerStart)
+    const isDeploying = !permalink && isFreshPR
+    const prDescriptionAbove = prBody?.split(markerStart)[0]?.trim() ?? ''
+    const prDescriptionBelow = prBody?.split(markerEnd).pop()?.trim() ?? ''
 
-    const body = `${prDescriptionAbove}
-${markerStart}
----
-🤖 **${domain} preview links**
-_Latest_: https://${branchName}.${domain}${!permalink && freshPR ? ' (Deploying... 🚧)' : ''}
-_Current permalink_: ${permalink ?? '(Deploying... 🚧)'}
-${markerEnd}
-${freshPR ? '' : prDescriptionBelow}`
+    const body = strip`
+        ${prDescriptionAbove}
+        ${markerStart}
+        ---
+        🤖 **${domain} preview links**
+        _Latest_: https://${branchName}.${domain}${isDeploying ? ' (Deploying... 🚧)' : ''}
+        _Current permalink_: ${permalink ?? '(Deploying... 🚧)'}
+        ${markerEnd}
+        ${isFreshPR ? '' : prDescriptionBelow}
+    `
 
     await octokit.request('PATCH /repos/{owner}/{repo}/pulls/{pull_number}', {
         ...repo,
-        pull_number: pr.number,
+        pull_number: prNumber,
         body
     })
 }
--- actions/s3-cache/restore.test.ts
+++ actions/s3-cache/restore.test.ts
@@ -1,3 +1,4 @@
+import {stripIndents as strip} from 'common-tags'
 import {restoreS3Cache} from './restore'
 import * as utils from '../utils'
 
@@ -8,55 +9,61 @@ jest.mock('../utils')
 beforeEach(() => jest.clearAllMocks())
 
 describe(`S3 Cache Action - Restore cache`, () => {
-    test(`
+    test(
+        strip`
         When a cache file in S3 doesn't exists
         Then it should return a "false" processed flag
         And it should return the S3 key and tree hash used 
-    `, async () => {
-        const treeHash = 'b017ebdf289ba78787da4e9c3291f0b7959e7059'
-        mockedUtils.getCurrentRepoTreeHash.mockResolvedValue(treeHash)
-        mockedUtils.fileExistsInS3.mockResolvedValue(false)
+        `,
+        async () => {
+            const treeHash = 'b017ebdf289ba78787da4e9c3291f0b7959e7059'
+            mockedUtils.getCurrentRepoTreeHash.mockResolvedValue(treeHash)
+            mockedUtils.fileExistsInS3.mockResolvedValue(false)
 
-        const output = await restoreS3Cache({
-            bucket: 'my-bucket',
-            keyPrefix: 'horse',
-            repo: {owner: 'my-org', repo: 'my-repo'}
-        })
+            const output = await restoreS3Cache({
+                bucket: 'my-bucket',
+                keyPrefix: 'horse',
+                repo: {owner: 'my-org', repo: 'my-repo'}
+            })
 
-        expect(output.key).toBe(`cache/my-org/my-repo/horse/${treeHash}`)
-        expect(output.processed).toBe(false)
-        expect(output.treeHash).toBe(treeHash)
+            expect(output.key).toBe(`cache/my-org/my-repo/horse/${treeHash}`)
+            expect(output.processed).toBe(false)
+            expect(output.treeHash).toBe(treeHash)
 
-        expect(mockedUtils.fileExistsInS3).toHaveBeenCalledTimes(1)
-        expect(mockedUtils.fileExistsInS3).toHaveBeenCalledWith({
-            bucket: 'my-bucket',
-            key: `cache/my-org/my-repo/horse/${treeHash}`
-        })
-    })
+            expect(mockedUtils.fileExistsInS3).toHaveBeenCalledTimes(1)
+            expect(mockedUtils.fileExistsInS3).toHaveBeenCalledWith({
+                bucket: 'my-bucket',
+                key: `cache/my-org/my-repo/horse/${treeHash}`
+            })
+        }
+    )
 
-    test(`
+    test(
+        strip`
         When a cache file in S3 already exists
         Then it should return a "true" processed flag
         And it should return the S3 key and tree hash used
-    `, async () => {
-        const treeHash = 'cba2d570993b9c21e3de282e5ba56d1638fb32de'
-        mockedUtils.getCurrentRepoTreeHash.mockResolvedValue(treeHash)
-        mockedUtils.fileExistsInS3.mockResolvedValue(true)
+        `,
+        async () => {
+            const treeHash = 'cba2d570993b9c21e3de282e5ba56d1638fb32de'
+            mockedUtils.getCurrentRepoTreeHash.mockResolvedValue(treeHash)
+            mockedUtils.fileExistsInS3.mockResolvedValue(true)
 
-        const output = await restoreS3Cache({
-            bucket: 'my-other-bucket',
-            keyPrefix: 'horse',
-            repo: {owner: 'my-org', repo: 'my-repo'}
-        })
+            const output = await restoreS3Cache({
+                bucket: 'my-other-bucket',
+                keyPrefix: 'horse',
+                repo: {owner: 'my-org', repo: 'my-repo'}
+            })
 
-        expect(output.key).toBe(`cache/my-org/my-repo/horse/${treeHash}`)
-        expect(output.processed).toBe(true)
-        expect(output.treeHash).toBe(treeHash)
+            expect(output.key).toBe(`cache/my-org/my-repo/horse/${treeHash}`)
+            expect(output.processed).toBe(true)
+            expect(output.treeHash).toBe(treeHash)
 
-        expect(mockedUtils.fileExistsInS3).toHaveBeenCalledTimes(1)
-        expect(mockedUtils.fileExistsInS3).toHaveBeenCalledWith({
-            bucket: 'my-other-bucket',
-            key: `cache/my-org/my-repo/horse/${treeHash}`
-        })
-    })
+            expect(mockedUtils.fileExistsInS3).toHaveBeenCalledTimes(1)
+            expect(mockedUtils.fileExistsInS3).toHaveBeenCalledWith({
+                bucket: 'my-other-bucket',
+                key: `cache/my-org/my-repo/horse/${treeHash}`
+            })
+        }
+    )
 })
