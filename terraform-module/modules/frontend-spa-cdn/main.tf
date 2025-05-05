5340d1e1c96aacc5bf349c642e3885fbf43859e4
Sergey Prylukin <sergey.prylukin@pleo.io>
2025-05-05 16:10:31 +0200

    Added custom headers config for /loading.html

--- terraform-module/modules/frontend-spa-cdn/main.tf
+++ terraform-module/modules/frontend-spa-cdn/main.tf
@@ -82,6 +82,32 @@ resource "aws_cloudfront_distribution" "this" {
     viewer_protocol_policy = "redirect-to-https"
   }
 
+  # Cache behaviour applying to specific path: /loading.html
+  # To allow sharing the browsing context group with the opener page
+  # This is needed to set up connection with 3rd party integrations
+  ordered_cache_behavior {
+    path_pattern     = "/loading.html"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.loading_integration_behaviour_headers_policy.id
+
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 1
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
   # Serve a custom 404 error page when requesting a file that doesn't exist in the bucket 
   custom_error_response {
     error_caching_min_ttl = 10
@@ -170,6 +196,64 @@ resource "aws_cloudfront_response_headers_policy" "default_behaviour_headers_pol
       header   = "Cross-Origin-Opener-Policy"
       override = true
       value    = "same-origin-allow-popups"
+    }
+  }
+}
+
+// This is custom headers policy for /loading.html page
+// This page is used as an intermediate page for 3rd party integration connections
+// And it should have ability to share same browsing context group with the opener page
+resource "aws_cloudfront_response_headers_policy" "loading_integration_behaviour_headers_policy" {
+  name = "${var.app_name}-default-headers-policy"
+  security_headers_config {
+    content_type_options {
+      override = true
+    }
+    frame_options {
+      frame_option = var.block_iframes ? "DENY" : "SAMEORIGIN"
+      override     = true
+    }
+    referrer_policy {
+      referrer_policy = "same-origin"
+      override        = true
+    }
+    xss_protection {
+      mode_block = true
+      protection = true
+      override   = true
+    }
+    strict_transport_security {
+      access_control_max_age_sec = "63072000"
+      include_subdomains         = true
+      preload                    = true
+      override                   = true
+    }
+  }
+
+  custom_headers_config {
+
+    // Adds cache control HTTP headers to the response to remove any caching
+    // Since we're only handling skeleton HTML files in this behaviour, disabling
+    // caching has little performance overhead. All static assets are cached aggressively
+    // in another behaviour.
+    items {
+      header   = "Cache-Control"
+      override = true
+      value    = "max-age=0,no-cache,no-store,must-revalidate"
+    }
+
+    // Adds robots tag HTTP header to the response to prevent indexing by bots (unless in production)
+    items {
+      header   = "X-Robots-Tag"
+      override = true
+      value    = var.is_robots_indexing_allowed && var.env == "production" ? "all" : "noindex, nofollow"
+    }
+
+    // using unsafe-none to explicitly allow sharing the browsing context group with the opener page
+    items {
+      header   = "Cross-Origin-Opener-Policy"
+      override = true
+      value    = "unsafe-none"
     }
   }
 }
